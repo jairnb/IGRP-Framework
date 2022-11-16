f372aba4716fe92b3f97628526fcf6addd0718c3
Pedro Fernandes <jair10.nb@hotmail.com>
2022-11-15 19:02:45 -0600

    authentication services start

---
+++ igrp-microservices/igrp-microservices/igrp-authorization-service/src/test/java/com/igrp/authorizationservice/IgrpAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igrp.authorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgrpAuthorizationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
